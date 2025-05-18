8e2bf1a1219ce8ba9debc6c421b62eeca9afa0c1
ikerb11 <ikerberna0@gmail.com>
2025-05-18 19:04:58 +0200

    Rutas APi funcionan

--- RecuperarServidor/routes/web.php
+++ RecuperarServidor/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\ProfileController;
+
+Route::apiResource('posts', PostController::class);
+Route::apiResource('profiles', ProfileController::class);
+Route::get('/test', function () {
+    return response()->json(['message' => 'API funcionando']);
+});
+
+
